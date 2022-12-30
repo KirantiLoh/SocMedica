a7523a50fe4ffe0d97a7f5a9e46d27c5a14c1441
Maurice Yang <mauriceyangmy28@gmail.com>
2022-12-30 20:33:17 +0700

    Changed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,19 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+// !process.env.SKIP_ENV_VALIDATION && 
+// (await import ("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  images: {
-    domains: ["cdn.discordapp.com"]
-  }
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    images: {
+        domains: ["cdn.discordapp.com"]
+    }
 };
 export default config;
